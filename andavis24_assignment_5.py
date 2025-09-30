3b60e10f974b41508b64678a0d97c8d02bc4677b
Ajani Davis <69487297+andavis24@users.noreply.github.com>
2025-09-29 20:32:55 -0400

    Challenge 2: Prime checker - for loop for known range

--- andavis24_assignment_5.py
+++ andavis24_assignment_5.py
@@ -1,3 +1,7 @@
+# COMP 163 - Assignment 4: College Life Adventure Game
+# Author: Ajani Davis
+# This program simulates college decisions using branching logic.
+
 print("=== Challenge 1: Collatz Conjecture ===")
 
 curent_number = int(input("Enter starting number: "))
@@ -12,4 +16,22 @@ while curent_number > 1:
     step_count = step_count + 1
 print(int(curent_number))
 print("Steps:",step_count)
+print()
+
+print("=== Challenge 2: Prime Number Checker ===")
+num = int(input("Enter a number: "))
+print(f"Testing divisors from 2 to {num - 1}...")
+
+test = num - 1
+
+while test >= 2:
+    if num % test == 0:
+        print(f"{num} is not prime (divisible by {test})")
+        break
+    else:
+        test = test - 1
+
+if test < 2:
+    print(f"{num} is prime!")
+
 print()
